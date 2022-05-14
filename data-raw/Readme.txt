e030c0aa95eb23504905e6b36a696eb6448910e3
DerPsychologe <103497963+DerPsychologe@users.noreply.github.com>
2022-05-14 11:09:06 +0200

    Delete data-raw directory

--- data-raw/Readme.txt
+++
@@ -1,1 +0,0 @@
-Das hier funktioniert noch gar nicht. Der ganze data-raw-Ordner wird nicht mirgenommen bzw. ich weiß nicht, wie ich darauf zugreifen könnte
--- data-raw/preprocess.R
+++
@@ -1,5 +0,0 @@
-# Load raw data from .sav file
-exampleData <- read_sav("data-raw/Lernkultur_Zusammengefügt.sav")
-# Apply preprocessing...
-# Save the cleaned data in the required R package location
-usethis::use_data(exampleData)
